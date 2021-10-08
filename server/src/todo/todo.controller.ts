154eea02ad93267d1ed7d85326a48fa30307b5b0
Никита Павлов <nikpav@Nikitas-MacBook-Pro.local>
2021-10-08 15:07:06 +0300

    server

---
+++ server/src/todo/todo.controller.ts
@@ -0,0 +1,76 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ITodo } from './model';
+
+let todos: ITodo[] = [
+  'NestJS',
+  'GraphQL',
+  'Apollo',
+  'TypeScript',
+  'React',
+  'Redux',
+  'React Query',
+  'Angular',
+  'Vue',
+  'D3',
+  'Svelte',
+  'SolidJS',
+  'NextJS',
+  'AWS',
+].map((text, index) => ({
+  id: index + 1,
+  text: `Learn ${text}`,
+  active: true,
+  done: false,
+}));
+
+@Controller('todo')
+export class TodoController {
+  @Get()
+  async index() {
+    return todos.find(({ active }) => active);
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: string): Promise<ITodo> {
+    return todos.find((todo) => todo.id === parseInt(id));
+  }
+
+  @Post()
+  async create(@Body() { text }: { text: string }): Promise<ITodo> {
+    const todo = {
+      id: todos.length + 1,
+      text,
+      active: true,
+      done: false,
+    };
+    todos.push(todo);
+    return todo;
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: ITodo): Promise<ITodo> {
+    todos = todos.map((todo) =>
+      todo.id === parseInt(id) ? { ...todo, ...data } : todo,
+    );
+
+    return data;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async delete(@Param('id') id: string): Promise<number> {
+    todos = todos.map((todo) =>
+      todo.id === parseInt(id) ? { ...todo, active: false } : todo,
+    );
+    return parseInt(id);
+  }
+}
